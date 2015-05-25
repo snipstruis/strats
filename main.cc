e9aac01c8dafbe423521e54da8aeb9b42a119c52
Douwe van Gijn <kipstad@hotmail.com>
2015-05-26 01:06:30 +0200

    cleaned up debug messages

--- main.cc
+++ main.cc
@@ -80,14 +80,13 @@ constexpr int max3(int const a, int const b, int const c){
 char assign_color(char other_color, char *buf, size_t size){
 	void* blue = memmem(buf,size,"BLUE",4);
 	void* red  = memmem(buf,size,"RED",3);
-	printf("checking colors: blue=%p red=%p\n",blue,red);
-	if((blue && !red) || (blue&&red&&(blue<red)) ){
+	if(blue && !red){
 		if(other_color!='B'){
 			return 'B';
 		}else{
 			return 'R';
 		}
-	}else if((red && !blue)||(blue&&red&&(red<blue))){
+	}else if(red && !blue){
 		if(other_color!='R'){
 			return 'R';
 		}else{
@@ -104,30 +103,29 @@ void handle_setup_messages(int fd, char* this_color, char* other_color, char* pi
 	if(*this_color==0){
 		// receive preferred color
 		int bytes_read = recv(fd, buf, sizeof(buf),0);
-		printf("received \"%.*s\" (expecting RED or BLUE)\n",bytes_read,buf);
 
 		*this_color = assign_color(*other_color, buf, bytes_read);
 		if(*this_color=='B'){
-			printf("sent \"BLUE\" to %d\n",fd);
+			printf("assigned BLUE to %d\n",fd);
 			send(fd,"BLUE",4,0);
 		}else if(*this_color=='R'){
-			printf("sent \"RED\" to %d\n",fd);
+			printf("assigned RED to %d\n",fd);
 			send(fd,"RED",3,0);
 		}else{
-			printf("sent \"INVALID_COLOR\" to %d\n",fd);
-			send(fd,"INVALID_COLOR",13,0);
+			send(fd,"INVALID",7,0);
 		}
 	}else if(*pieces==0){
 		// validate pieces
 		int bytes_read = recv(fd, buf, sizeof(buf),0);	
 		if(validate_pieces(buf,bytes_read)){
 			memcpy(pieces,buf,40);
+			printf("received valid piece setup from %d\n",fd);
 			send(fd,"OK",2,0);
 		}else{
-			send(fd,"INVALID_PIECE_SETUP",19,0);
+			send(fd,"INVALID",7,0);
 		}
 	} else {
-		send(fd,"Please_wait_for_other_player_to_finish",38,0);
+		// ignore other input
 		recv(fd, buf, sizeof(buf),0);
 	}
 }
@@ -137,10 +135,8 @@ int handle_new_connections(int sockfd){
 	socklen_t sin_size = sizeof their_addr;
 	int fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
 	if(fd == -1) {
-		perror("accept failed on fd");
 		return 0;
 	}else{
-		printf("fd accepted (fd=%d)\n",fd);
 		return fd;
 	}
 }
@@ -162,7 +158,6 @@ setup_info setup_game(int sockfd){
 	memset(pieces,0,80); // zero initialize pieces
 
 	while(true){
-		printf(".");
 		fd_set tmp = master;
 		int const maxfd = max3(sockfd,fd[0],fd[1]);
 		int sel = select(maxfd+1, &tmp, NULL, NULL, NULL);
@@ -202,7 +197,6 @@ setup_info setup_game(int sockfd){
 						exit(-1);
 					}
 				}else{
-					printf("hier!!!");
 					int current = fd[0]==i?0:1;
 					int other   = fd[0]==i?1:0;
 					handle_setup_messages(i,&color[current],&color[other],pieces[current]);
@@ -212,8 +206,6 @@ setup_info setup_game(int sockfd){
 					&& color[0]!=0     && color[1]!=0
 					&& pieces[0][0]!=0 && pieces[1][0]!=0){
 						// return it in a more convenient format
-						printf("all initialisation data received!");
-
 						int blue,red;
 						if(color[0]=='R'){ red=0; blue=1; }
 						else { red=1; blue=0;}
@@ -256,7 +248,7 @@ int main(int argc, char** argv){
 	print_red_map(pmap);
 	write(1,pmap,110);
 
-	puts("blue map");
+	puts("\nblue map");
 	print_blue_map(pmap);
 	write(1,pmap,110);
 
